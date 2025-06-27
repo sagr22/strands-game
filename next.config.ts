1286bba5f0e8dbfae0f8ae2914547a835f0beecf
Suhani Agrawal <suhani@terpmail.umd.edu>
2025-06-27 14:58:57 +0530

    Fix 1

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/strands-game',
-  assetPrefix: '/strands-game',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
